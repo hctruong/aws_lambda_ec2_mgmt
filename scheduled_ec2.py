ff1f1e9735004f37dcca4ff12996f27b2371a5f4
Huan Truong <huan.truong@gmail.com>
2021-04-01 22:24:32 -0500

    Update scheduled_ec2.py
    fixed some logic with tags

--- scheduled_ec2.py
+++ scheduled_ec2.py
@@ -42,34 +42,45 @@ def lambda_handler(event, context):
 	
 	# Sorting tags instances if scheduled time is met
 	
+	startTime = ''
+	stopTime = ''
+	excludeDay = ''
+	
 	stop_instances = []
 	start_instances = []
 	excluded_days = []
 	skip = False
 	
 	for instance in instances:
+	
 		for tag in instance.tags:
 			if tag['Key'] == 'ExcludedDays':
-				
-				# Spliting the excluded String in separated values
-				excluded_days[:] = tag['Value']
-				
-				# Don't do anything if exclude day is the current day
-				for day in excluded_days:
-					if day == current_day:
-						skip = True
-						break
+				excludeDay = tag['Value']
+			if tag['Key'] == 'ScheduleStop':
+				stopTime = tag['Value']
+			if tag['Key'] == 'ScheduleStart':
+				startTime = tag['Value']
+
+		
+		if excludeDay != '':
+			# Spliting the excluded String in separated values
+			excluded_days[:] = excludeDay
+			# Don't do anything if exclude day is the current day
+			for day in excluded_days:
+				if days[day] == current_day:
+					skip = True
+					break
 			
-			if not skip: 
-				if tag['Key'] == 'ScheduleStop':
-					stop_time = tag['Value'].split(':')
-					if stop_time[0] == current_time:
-						stop_instances.append(instance.id)
-						
-				if tag['Key'] == 'ScheduleStart':
-					start_time = tag['Value'].split(':')
-					if start_time[0] == current_time:
-						start_instances.append(instance.id)
+		if not skip: 
+			if stopTime != '':
+				stop_time = stopTime.split(':')
+				if stop_time[0] == current_time:
+					stop_instances.append(instance.id)
+					
+			if startTime != '':
+				start_time = startTime.split(':')
+				if start_time[0] == current_time:
+					start_instances.append(instance.id)
 
 	# Shutdowning/Starting Up instances
 	if len(stop_instances) > 0:
